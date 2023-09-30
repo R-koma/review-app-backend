8d5b5be0ec6419996f4ff26e01f241e20fa0dc9c
R-koma <ryomakomazawa@gmail.com>
2023-09-30 12:57:58 +0900

    fix MongoDB

--- controllers/reviewControllers.js
+++ controllers/reviewControllers.js
@@ -1,11 +1,31 @@
 const Review = require("./models/review");
+const { calculateInterval } = require("./path-to-your-calculateInterval-file");
 
 exports.completeReview = async (req, res) => {
-  // ... (ロジック定義)
+  const { reviewId } = req.body;
+
+  // 復習項目を取得
+  const review = await Review.findById(reviewId);
+
+  // 復習回数をインクリメント
+  review.reviewCount += 1;
+
+  // 最終復習日を更新
+  review.lastReviewed = new Date();
+
+  // 次の復習日を計算（ここで忘却曲線のロジックを適用）
+  const interval = calculateInterval(review.reviewCount); // この関数は忘却曲線のロジックに基づいて間隔を計算します
+  review.nextReviewDate = new Date(Date.now() + interval);
+
+  // データベースを更新
+  await review.save();
+
+  res.status(200).send(review);
 };
 
 exports.getReviews = async (req, res) => {
-  // ... (ロジック定義)
+  const reviews = await Review.find({ nextReviewDate: { $lte: new Date() } });
+  res.status(200).send(reviews);
 };
 
 // 他の関連するコントローラー関数もここに定義する
--- utils/intervalCalculator.js
+++ utils/intervalCalculator.js
@@ -1,1 +1,5 @@
 //忘却曲線のアルゴリズムを構築予定。
+export function calculateInterval(reviewCount) {
+  // これは単純な例で、実際には忘却曲線のロジックに基づいて間隔を計算する必要があります。
+  return 86400000 * Math.pow(2, reviewCount); // 1日、2日、4日、8日、...と復習間隔が増えていく単純な例
+}
